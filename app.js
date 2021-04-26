cd58af065b133cf0498cb6e74c5234a935d8f808
Amoo Pedram <Pedroxam@gmail.com>
2021-04-26 22:15:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -44,4 +44,47 @@
 				$(that).removeClass('disabled');
 			})
 		})
+		
+		/**
+		* Callback Checker
+		*/
+		$('#callback_check').click(function(){
+			
+			if($('#callback_c').val() == "") {
+				return alert('Please Enter Callback URL');
+			}
+			if($('#amount').val() == "") {
+				return alert('Please Enter Amount value');
+			}
+			if($('#txid_in').val() == "") {
+				return alert('Please Enter txid');
+			}
+			if($('#confirmations').val() == "") {
+				return alert('Please Enter confirmations');
+			}
+			
+			var callback = $('#callback_c').val();
+			var amount = $('#amount').val();
+			var coin = $('#coin_c').val();
+			var txid_in = $('#txid_in').val();
+			var confirmations = $('#confirmations').val();
+			
+			let callbackURL = `${callback}?value_coin={$amount}&coin={$coin}&txid_in={$txid_in}&confirmations=${confirmations}`;
+			
+			var that = this;
+			
+			$(that).addClass('disabled');
+			$('#waiting_c').show();
+			
+			$.ajax({
+				type:'GET',
+				url: callbackURL
+			})
+			.done(function(response){
+				$('#result_c').show();
+				$('#waiting_c').hide();
+				$('#response_c').html(response);
+				$(that).removeClass('disabled');
+			})
+		})
 	});
